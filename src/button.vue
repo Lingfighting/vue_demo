6f76890c755bde38dfdb5c8cac914c0f7c72aa96
liying66 <liying@romens.cn>
2018-08-16 16:19:50 +0800

    feat(局部): 将button组件改为单文件组件，使用parcel启动项目

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+  export default {
+  
+  }
+</script>
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;   /*不直接设置按钮的宽度，用padding来做*/
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    };
+    &:active {
+      background: var(--button-active-bg);
+    };
+    &:focus {
+      outline: none;
+    };
+  }
+</style>
