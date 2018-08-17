757109477275b6559a6768e7ac2cce12848251cb
liying66 <liying@romens.cn>
2018-08-17 14:02:38 +0800

    feat(局部): 用css设置button里面icon的位置

--- src/button.vue
+++ src/button.vue
@@ -1,20 +1,38 @@
 <template>
-  <button class="g-button">
+  <!--变量不能作为key，用"[ ]"计算出变量的值，再作为key-->
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <svg v-if="icon" class="icon">
-      <use :xlink:href=`#i-${icon}`></use>
+      <use :xlink:href="`#i-${icon}`"></use>
     </svg>
-    <slot></slot>
+    <!--不能直接给slot加class-->
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 <script>
   export default {
-     props: ['icon']
+    props: {
+      icon: {},
+      loading: {
+        type: Boolean,
+        default: false
+      },
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator (value) {
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
   }
 </script>
 <style lang="scss">
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
+    weight: 300px;
     padding: 0 1em;   /*不直接设置按钮的宽度，用padding来做*/
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
@@ -28,5 +46,25 @@
     &:focus {
       outline: none;
     };
+    // 使用弹性盒子，才能使用 order
+    display: inline-flex; justify-content: center; align-items: center;
+    > .content {
+      order: 2;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .4em;
+    }
+    &.icon-right{
+      > .content {
+        order: 1;
+      }
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .4em;
+      }
+    }
+    vertical-align: middle;
   }
 </style>
